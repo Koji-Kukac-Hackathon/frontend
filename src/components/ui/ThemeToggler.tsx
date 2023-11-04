cabafdb1eb7ed66ecf2199e9bf467a58fb0f361e
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-11-04 12:58:07 +0100

    Theme and components

--- src/components/ui/ThemeToggler.tsx
+++ src/components/ui/ThemeToggler.tsx
@@ -2,11 +2,22 @@
 
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 
 function ThemeToggle() {
   const { resolvedTheme, setTheme } = useTheme()
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <Button
